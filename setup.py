b42a122509fcc09e6bb3ba4e554c75ae1a70efbf
Jeremy Magland <jmagland@flatironinstitute.org>
2023-03-27 12:17:01 -0400

    include .h files

--- setup.py
+++ setup.py
@@ -3,19 +3,6 @@ from setuptools import setup, find_packages
 # Available at setup time due to pyproject.toml
 from pybind11.setup_helpers import Pybind11Extension
 
-class get_pybind_include(object):
-    """Helper class to determine the pybind11 include path
-    The purpose of this class is to postpone importing pybind11
-    until it is actually installed, so that the ``get_include()``
-    method can be invoked. """
-
-    def __init__(self, user=False):
-        self.user = user
-
-    def __str__(self):
-        import pybind11
-        return pybind11.get_include(self.user)
-
 ext_modules = [
     Pybind11Extension(
         "isosplit6_cpp",
@@ -26,11 +13,6 @@ ext_modules = [
             'src/isosplit5.cpp',
             'src/isocut5.cpp',
             'src/jisotonic5.cpp'
-        ],
-        include_dirs=[
-            # Path to pybind11 headers
-            get_pybind_include(),
-            get_pybind_include(user=True)
         ]
     )
 ]
